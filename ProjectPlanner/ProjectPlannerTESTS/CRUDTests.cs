a7b91c6a220bf24b3654d063149f16757af96b6e
Alex Griffin <alexgriffin.dev@gmail.com>
2020-11-04 13:26:47 +0000

    Added Test Project
    Added empty test project to solution

---
+++ ProjectPlanner/ProjectPlannerTESTS/CRUDTests.cs
@@ -0,0 +1,18 @@
+using NUnit.Framework;
+
+namespace ProjectPlannerTESTS
+{
+    public class CRUDTests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
