64b807220a9457083cf254fda0fb433c11aee3ee
Alex Griffin <alexgriffin.dev@gmail.com>
2020-11-04 13:22:07 +0000

    Created Database
    Initial create of the Project Planner database

---
+++ ProjectPlanner/ProjectPlannerModel/Migrations/20201104132023_InitialCreate.cs
@@ -0,0 +1,113 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ProjectPlannerModel.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Notes",
+                columns: table => new
+                {
+                    NoteId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Title = table.Column<string>(nullable: true),
+                    Body = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Notes", x => x.NoteId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Projects",
+                columns: table => new
+                {
+                    ProjectId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Title = table.Column<string>(nullable: true),
+                    Description = table.Column<string>(nullable: true),
+                    Status = table.Column<int>(nullable: false),
+                    Link = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Projects", x => x.ProjectId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Features",
+                columns: table => new
+                {
+                    FeatureId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Title = table.Column<string>(nullable: true),
+                    Description = table.Column<string>(nullable: true),
+                    ProjectId = table.Column<int>(nullable: false),
+                    Status = table.Column<int>(nullable: false),
+                    Priority = table.Column<int>(nullable: false),
+                    Notes = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Features", x => x.FeatureId);
+                    table.ForeignKey(
+                        name: "FK_Features_Projects_ProjectId",
+                        column: x => x.ProjectId,
+                        principalTable: "Projects",
+                        principalColumn: "ProjectId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Issues",
+                columns: table => new
+                {
+                    IssueId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Title = table.Column<string>(nullable: true),
+                    Description = table.Column<string>(nullable: true),
+                    ProjectId = table.Column<int>(nullable: false),
+                    Status = table.Column<int>(nullable: false),
+                    Priority = table.Column<int>(nullable: false),
+                    Notes = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Issues", x => x.IssueId);
+                    table.ForeignKey(
+                        name: "FK_Issues_Projects_ProjectId",
+                        column: x => x.ProjectId,
+                        principalTable: "Projects",
+                        principalColumn: "ProjectId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Features_ProjectId",
+                table: "Features",
+                column: "ProjectId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Issues_ProjectId",
+                table: "Issues",
+                column: "ProjectId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Features");
+
+            migrationBuilder.DropTable(
+                name: "Issues");
+
+            migrationBuilder.DropTable(
+                name: "Notes");
+
+            migrationBuilder.DropTable(
+                name: "Projects");
+        }
+    }
+}
--- ProjectPlanner/ProjectPlannerModel/PlannerModel.cs
+++ ProjectPlanner/ProjectPlannerModel/PlannerModel.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Dynamic;
 using System.Text;
 using Microsoft.EntityFrameworkCore;
@@ -19,21 +21,45 @@ namespace ProjectPlannerModel
 
     public class Project
     {
-
+        public int ProjectId { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public int Status { get; set; }
+        public string Link { get; set; }
+        public List<Feature> Features { get; } = new List<Feature>();
+        public List<Issue> Issues { get; } = new List<Issue>();
     }
 
     public class Feature
     {
+        public int FeatureId { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
 
+        [ForeignKey("ProjectId")]
+        public int ProjectId { get; set; }
+        public int Status { get; set; }
+        public int Priority { get; set; }
+        public string Notes { get; set; }
     }
 
     public class Issue
     {
+        public int IssueId { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
 
+        [ForeignKey("ProjectId")]
+        public int ProjectId { get; set; }
+        public int Status { get; set; }
+        public int Priority { get; set; }
+        public string Notes { get; set; }
     }
 
     public class Note
     {
-
+        public int NoteId { get; set; }
+        public string Title { get; set; }
+        public string Body { get; set; }
     }
 }
