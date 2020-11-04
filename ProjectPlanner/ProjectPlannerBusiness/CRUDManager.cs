88f2e25d55a8bd4ee2869c7ebd4442f2f69ddc36
Alex Griffin <alexgriffin.dev@gmail.com>
2020-11-04 13:24:50 +0000

    Added Business Layer
    Added business layer to project, currently empty

---
+++ ProjectPlanner/ProjectPlannerBusiness/CRUDManager.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ProjectPlannerBusiness
+{
+    class CRUDManager
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
